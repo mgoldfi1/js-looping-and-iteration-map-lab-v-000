76ec7df8468170ef4038e30c5520244cb0acc993
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-06 21:15:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ function lowerCaseDrivers(drivers) {
 }
 
 function nameToAttributes(drivers) {
-let newArray = []
- for (const name of drivers) {
-    newArray.push(Object.assign({},{firstName: name.split(" ")[0], lastName: name.split(" ")[1]}))
- }
- return newArray
+// let newArray = []
+//  for (const name of drivers) {
+//     newArray.push(Object.assign({},{firstName: name.split(" ")[0], lastName: name.split(" ")[1]}))
+//  }
+
+ return drivers.map(function(x) {return Object.assign({}, {firstName: name.split(" ")[0], lastName: name.split(" ")[1]}) })
 }
